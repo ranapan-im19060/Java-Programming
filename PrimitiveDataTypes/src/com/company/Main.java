f887eb5f01c54695f5d125df79d645b53756fb75
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-11-17 19:57:13 +0530

    Primitive DataTypes

---
+++ PrimitiveDataTypes/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int maxValue = Integer.MAX_VALUE;
+        int minValue = Integer.MIN_VALUE;
+        System.out.println("Max Value "+maxValue+" Min Value "+minValue);
+
+        byte maxByteValue = Byte.MAX_VALUE;
+        byte minByteValue = Byte.MIN_VALUE;
+        System.out.println("Max Value "+maxByteValue+" Min Value "+minByteValue);
+
+        short maxShortValue = Short.MAX_VALUE;
+        short minShortValue = Short.MIN_VALUE;
+        System.out.println("Max Value "+maxShortValue+" Min Value "+minShortValue);
+
+        long maxLongValue = Long.MAX_VALUE;
+        long minLongValue = Long.MIN_VALUE;
+        System.out.println("Max Value "+maxLongValue+" Min Value "+minLongValue);
+
+        long value = 100L; //You need to put 'L' after long value.
+        System.out.println(value);
+
+        
+    }
+}
