ddd0b41bc92a0450bbf03df9b6524cea90101e7b
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-11-17 18:32:59 +0530

    commi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       print();
+    }
+
+    public static void print(){
+        int first = 10;
+        int second = 30;
+        int output = first + second;
+
+        System.out.println("Hello World "+ output);
     }
 }
