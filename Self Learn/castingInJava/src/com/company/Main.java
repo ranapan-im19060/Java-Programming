3b94abcdee52fe99cc637287a87ad69c794ebf35
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-11-28 16:15:56 +0530

    11/28

---
+++ Self Learn/castingInJava/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int value = 29;
+	    int x = value/2;
+	    byte z = (byte)(value/2);
+
+	    long e = value;
+		System.out.println(e);
+
+		short w = (short)(value);
+		System.out.println(w);
+		char v = 'c';
+		int g = (v);
+		System.out.println(g);
+
+		int a = 5;
+		int y = 6;
+
+		System.out.println(y/a); //integer division
+
+		double b = a;
+		double c = y;
+		System.out.println(c/b);
+
+
+
+    }
+}
